f7eedffe3426987f83ed575a3bfa54916d1629ed
kleborp <twozero@gmail.com>
2016-08-25 18:52:59 +0100

    Updated readme, added sofware module

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ Project Structure:
 - bin\ - Folder that will hold all scripts and dependencies
 - bin\scc-log.ps1 - Incomplete port of scc-log.pl, invokes the collector and tries to validate the output
 - bin\scc-collector.ps1 - Nearly complete port of scc-collector.pl, invokes all modules and keeps track of module timings and statistics
-- bin\modules\*.ps1 - Port of the scc_modules\*.pl, not all modules ported yet but the ones that have been should be complete and nearly identical in output to the original scripts
+- bin\modules\scc*.ps1 - Port of the "scc_modules\\*.pl" module files, not all modules ported yet but the ones that have been should be complete and nearly identical in output to the original scripts
 - data\ - Folder where output will be saved
 - tmp\ - Folder where temporary data may be written during collection
+
+# Port notes
+- Some WMI classes are deprecated and have therefore not been ported across, replacement functionality will be added later on after initial port is complete
+- The original Perl code is hard to read/follow in places, some liberty was taken in achieving the same result rather than providing an exact port
+- Scripts were written and tested on Windows 7, Windows 10 and Windows Server 2012 R2, no other system has been tested
+- It should be possible to run each script on it's own, everything uses "Write-Output" so you can see it when run from a prompt, redirect it to a file, or pipe it to another cmdlet
+- After the initial port is complete the scripts will probably be improved and partially rewritten, but the initial aim is to simply get a working port
---
+++ bin/modules/scc_0600_s_software.ps1
@@ -0,0 +1,98 @@
+﻿$computer = $env:COMPUTERNAME
+
+# Import generic functions
+$currentPath = $MyInvocation.MyCommand.Path | Split-Path
+Import-Module $currentPath\include\sccFunctions.psm1
+
+# Get some general network related information
+# Reference: https://msdn.microsoft.com/en-us/library/aa394102(v=vs.85).aspx
+$cs = Get-WmiObject -class Win32_ComputerSystem -computername $computer
+
+# We'll store installed software in this:
+$installedSoftware = @{}
+
+# Loop over both 32bit and 64bit registry keys
+foreach ($regPath in @('', 'Wow6432Node\')) {
+    
+    # Build key depending on bitness
+    $uninstallKey = (”SOFTWARE\" + $regPath + "Microsoft\Windows\CurrentVersion\Uninstall”)
+    Write-Output ("hlp:network:general::Data from registry: HKLM:\" + $uninstallKey)
+
+    # Open registry key
+    $registryObject =[microsoft.win32.registrykey]::OpenRemoteBaseKey(‘LocalMachine’,$computer)
+    $registryKey = $registryObject.OpenSubKey($uninstallKey)
+    # Get all subkeys under the uninstall key
+    $registrySubKeys = $registryKey.GetSubKeyNames()
+
+
+    # Loop over each subkey under Uninstall and add to installedSoftware
+    $i = $installedSoftware.Count
+    foreach($key in $registrySubKeys) {
+        $thiskey = $uninstallKey + "\\" + $key
+        $thisSubKey = $registryObject.OpenSubKey($thiskey)
+
+        $displayName = $thisSubKey.GetValue("DisplayName")
+        if($displayName) {
+            $installedSoftware[$i] = @{}
+            $installedSoftware[$i]["DisplayName"] = $displayName
+            # Remove funky characters from publisher name (Microsoft likes putting null characters in here)
+            $publisher = $thisSubKey.GetValue("Publisher") -Replace '[\W]', ''
+            $installedSoftware[$i]["Publisher"] = $publisher
+            $installedSoftware[$i]["DisplayVersion"] = $thisSubKey.GetValue("DisplayVersion")
+            $installedSoftware[$i]["InstallDate"] = $thisSubKey.GetValue("InstallDate")
+            $installedSoftware[$i]["InstallLocation"] = $thisSubKey.GetValue("InstallLocation")
+            $installedSoftware[$i]["NoModify"] = $thisSubKey.GetValue("NoModify")
+            $installedSoftware[$i]["NoRemove"] = $thisSubKey.GetValue("NoRemove")
+            $installedSoftware[$i]["NoRepair"] = $thisSubKey.GetValue("NoRepair")
+            $installedSoftware[$i]["URLInfoAbout"] = $thisSubKey.GetValue("URLInfoAbout")
+            $installedSoftware[$i]["WindowsInstaller"] = $thisSubKey.GetValue("WindowsInstaller")
+            $i++
+        }
+    }
+}
+
+# Output data for each product we found
+# TODO: Sort this by DisplayName somehow
+foreach ($key in $installedSoftware.Keys) {
+    # Remove : from DisplayName
+    $displayName = $installedSoftware[$key]["DisplayName"] -replace ":","_"
+    $label = ("fix:software:product:" + $displayName)
+    Write-Output ($label + "::Publisher:" + $installedSoftware[$key]["Publisher"])
+    Write-Output ($label + "::DisplayVersion:" + $installedSoftware[$key]["DisplayVersion"])
+    Write-Output ($label + "::InstallDate:" + $installedSoftware[$key]["InstallDate"])
+    Write-Output ($label + "::InstallLocation:" + $installedSoftware[$key]["InstallLocation"])
+    Write-Output ($label + "::NoModify:" + $installedSoftware[$key]["NoModify"])
+    Write-Output ($label + "::NoRemove:" + $installedSoftware[$key]["NoRemove"])
+    Write-Output ($label + "::NoRepair:" + $installedSoftware[$key]["NoRepair"])
+    Write-Output ($label + "::URLInfoAbout:" + $installedSoftware[$key]["URLInfoAbout"])
+    Write-Output ($label + "::WindowsInstaller:" + $installedSoftware[$key]["WindowsInstaller"])
+    
+}
+# Clear this in case we re-run the script in the same scope
+Remove-Variable installedSoftware
+$OSkey = Get-ProductKey()
+Write-Output ("fix:software:product keys:OS:" + $OSkey)
+
+function Get-ProductKey {
+    $map="BCDFGHJKMPQRTVWXY2346789" 
+    $value = (get-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").digitalproductid[0x34..0x42]  
+    $ProductKey = ""  
+    for ($i = 24; $i -ge 0; $i--) { 
+      $r = 0 
+      for ($j = 14; $j -ge 0; $j--) { 
+        $r = ($r * 256) -bxor $value[$j] 
+        $value[$j] = [math]::Floor([double]($r/24)) 
+        $r = $r % 24 
+      } 
+      $ProductKey = $map[$r] + $ProductKey 
+      if (($i % 5) -eq 0 -and $i -ne 0) { 
+        $ProductKey = "-" + $ProductKey 
+      } 
+    } 
+    $ProductKey
+}
+
+# TODO
+function Get-OfficeKey {
+    
+}
